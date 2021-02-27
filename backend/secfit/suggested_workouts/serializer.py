d8af08b02a3ae44a49b054cf8705e6c79ca32200
Victoria <victorah@stud.ntnu.no>
2021-02-27 20:26:44 +0100

    Small changes in serializer.

--- backend/secfit/suggested_workouts/serializer.py
+++ backend/secfit/suggested_workouts/serializer.py
@@ -1,14 +1,11 @@
 from rest_framework import serializers
 from .models import SuggestedWorkout
 from users.models import User
-from workouts.serializers import WorkoutFileSerializer
+from workouts.serializers import WorkoutFileSerializer, ExerciseInstanceSerializer
 
 
 class SuggestedWorkoutSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = SuggestedWorkout
-        suggested_workout_files = WorkoutFileSerializer(
-            many=True, required=False)
         fields = ['id', 'athlete', 'name', 'notes', 'date',
                   'status', 'suggested_exercise_instances', 'suggested_workout_files']
